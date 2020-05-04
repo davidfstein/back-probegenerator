c35209604b49d9d0d8914315b4fd0177bdf7a7a6
davidfstein <davidmfstein@gmail.com>
2020-05-04 14:09:29 -0400

    url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Constants = require('./utils/Constants');
 app.use(cors({credentials: true, origin: true}));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", `${Constants.FRONTEND_URL}/*`);
+    res.header("Access-Control-Allow-Origin", `${Constants.FRONTEND_URL}`);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
