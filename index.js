4cca572e0047441e30b4ed7b28903f29fec565dc
davidfstein <davidmfstein@gmail.com>
2020-05-04 12:55:51 -0400

    restrict cors

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const formidable = require('express-formidable');
+const Constants = require('./Constants');
 
 app.use(cors({credentials: true, origin: true}));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", `${Constants.FRONTEND_URL}/*`);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
