1fe01b907f19ef9549e3c1b4b0d5e9d7871d486b
David Stein <davidmfstein@gmail.com>
2020-07-17 13:01:50 -0400

    change region

--- utils/cloudFormationUtils.js
+++ utils/cloudFormationUtils.js
@@ -4,7 +4,7 @@ const Constants = require('./Constants');
 class CloudFormationUtils {
 
     constructor() {
-        this.cloudFormation = new AWS.CloudFormation({apiVersion: '2016-11-15', region:'us-east-2'});
+        this.cloudFormation = new AWS.CloudFormation({apiVersion: '2016-11-15', region:'us-east-1'});
     }
     
     launchStack(stackName, indexKey) {
